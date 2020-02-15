f83420075b027378f6c0015c96243bde0ec14406
Hibah Shafi <hibahshafi.hs@gmail.com>
2020-02-15 17:36:05 -0600

    Adding changes

---
+++ client/build/precache-manifest.d4fd8d27901e25d9b0d7be70cb7c51b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "000fe76b97fe294f3127",
+    "url": "/static/js/main.0faba6a0.chunk.js"
+  },
+  {
+    "revision": "3070b4aec8da0e6e7adb",
+    "url": "/static/js/2.a9a10943.chunk.js"
+  },
+  {
+    "revision": "000fe76b97fe294f3127",
+    "url": "/static/css/main.da51a160.chunk.css"
+  },
+  {
+    "revision": "977283ab3330472f9cc46235f8432e95",
+    "url": "/index.html"
+  }
+];
